815535e9106dbd0cc57c1b696214fd645e967e5c
Jakob <jake.thewoz@gmail.com>
2025-12-06 11:07:33 +0100

    part two is done

--- 6.py
+++ 6.py
@@ -1,4 +1,5 @@
 import data_getter
+import math
 
 data = data_getter.get_data(6).splitlines()
 
@@ -24,7 +25,6 @@ for i in range(len(homework[0])):
         add = False
         column = 1
 
-
     for j in range(4):
         if add:
             column += int(homework[j][i])
@@ -34,3 +34,38 @@ for i in range(len(homework[0])):
     total += column
 
 print(f'The grand total is {total}')
+
+# Part Two
+
+# Wow, kind of tricky.
+# I think we can loop through every character in one row,
+# collecting the numbers and combining when we find the operator.
+
+homework = data[:]
+numbers = []
+total = 0
+
+# Our main loop will go backwards through every character in a row
+for i in range(len(homework[0])-1, -1, -1):
+    number = ''
+
+    # Here we go down the columns for each row-character
+    for j in range(len(homework)-1):
+        number += homework[j][i]
+
+    # And if it's not a blank space, we add the number to our list
+    if number.strip() != '':
+        numbers.append(int(number))
+
+    # If the final row has an operator, we add or mult them up
+    if homework[4][i] != ' ':
+        # Go time!
+        if homework[4][i] == '+':
+            total += sum(numbers)
+        else:
+            total += math.prod(numbers)
+
+        # Now we reset numbers
+        numbers = []
+
+print(f'The new grand total is {total}')
