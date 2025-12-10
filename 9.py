0a2cb359726310d976e2b991204df59b28ffee67
Jakob <jake.thewoz@gmail.com>
2025-12-10 08:30:24 +0100

    starting a new approach

--- 9.py
+++ 9.py
@@ -1,6 +1,5 @@
 import data_getter
 from collections import deque
-import numpy as np
 
 data = data_getter.get_data(9).splitlines()
 
@@ -23,93 +22,38 @@ print(f'The largest area is {max(areas.values())}')
 
 # Part Two
 
-# I have no idea how to solve this, but...
-# I could try making the grid and filling in the green tiles,
-# then I could check each area manually?
+# New approach!
+# Let's 'compress' the grid down, look for the biggest area,
+# then look up the true area with the original tiles.
 
-# What if I skip the grid entirely, and just use sets?
+# First, let's compress
+unique_numbers = set()
+for x, y in red_tiles:
+    unique_numbers.add(x)
+    unique_numbers.add(y)
 
-x_max = max(tile[0] for tile in red_tiles)
-y_max = max(tile[1] for tile in red_tiles)
-x_min = min(tile[0] for tile in red_tiles)
-y_min = min(tile[1] for tile in red_tiles)
+sorted_unique = sorted(list(unique_numbers))
 
-walls = set()
+# Now we'll create a sort of compression map, where we map the 
+# original values to the 'compressed' indexes
 
-# Now let's create the borders
+compression_map = {
+    original: index
+    for index, original in enumerate(sorted_unique)
+}
 
-for i in range(len(red_tiles)):
-    x = red_tiles[i][0]
-    y = red_tiles[i][1]
+# Now we can create a compressed version of the original tiles!
 
-    walls.add((x,y))
+c_tiles = []
+for x, y in red_tiles:
+    c_x = compression_map[x]
+    c_y = compression_map[y]
 
-    x1 = red_tiles[i-1][0]
-    y1 = red_tiles[i-1][1]
+    c_tiles.append((c_x, c_y))
 
-    if x1 == x:
-        small_y, big_y = sorted([y, y1])
-        for j in range(small_y + 1, big_y):
-            walls.add((x,j))
-    else:
-        small_x, big_x = sorted([x, x1])
-        for j in range(small_x + 1, big_x):
-            walls.add((j,y))
+# Now that we have a compressed set of tiles, we can create a grid without
+# breaking the computer
 
-print('done painting #s and Xs')
-# Okay... instead of filling the inside, I'll flood the outside with Os
+grid = [['.' for _ in range(len(unique_numbers)+1)] for _ in range(len(unique_numbers)+1)]
 
-queue = deque([(x_min,y_min)]) # start at the upper left corner, which we know is '.'
 
-directions = [(1,0),(-1,0),(0,1),(0,-1)]
-
-outside = set()
-
-while queue:
-    x, y = queue.popleft()
-
-    # fill the space
-    outside.add((x,y))
-
-    # Add valid neighbors to the queue
-    for dx, dy in directions:
-        new_y, new_x = dy + y, dx + x
-
-        # Boundary check
-        if y_min-1 <= new_y < y_max+1 and x_min-1 <= new_x < x_max+1:
-            # Content check
-            if (new_x, new_y) not in outside \
-                    and (new_x, new_y) not in walls:
-                queue.append((new_x, new_y))
-
-print('done painting Os')
-# Now we should have a grid that has the outside filled
-# so any valid rectangle will be made up of only #, X, and ., (no Os)
-
-valid_areas = {}
-current_max = 0
-
-for i in range(len(red_tiles)):
-    for j in range(i+1, len(red_tiles)):
-
-        x = sorted((red_tiles[i][0], red_tiles[j][0]), reverse=True)
-        y = sorted((red_tiles[i][1], red_tiles[j][1]), reverse=True)
-
-        if (x[0] - x[1] + 1) * (y[0] - y[1] + 1) < current_max:
-            continue
-
-        valid = True
-        for k in range(x[1], x[0]+1):
-            if (k, y[0]) in outside or (k, y[1]) in outside:
-                valid = False
-                break
-        for k in range(y[1], y[0]+1):
-            if (x[0], k) in outside or (x[1], k) in outside:
-                valid = False
-                break
-
-        if valid:
-            current_max = (x[0] - x[1] + 1) * (y[0] - y[1] + 1)
-            valid_areas[(i, j)] = (x[0] - x[1] + 1) * (y[0] - y[1] + 1)
-
-print(f'The largest valid area is {current_max}')
